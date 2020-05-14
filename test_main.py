430795fa6a7353fa6cc0b084f469a5ade6f89ecf
Helena <helena.mkopp@gmail.com>
2020-05-14 15:24:43 -0300

    Final Project

--- test_main.py
+++ test_main.py
@@ -10,15 +10,9 @@ if __name__ == '__main__':
     manager2 = Manager("Isabel", "333.333.333-33", "5000.00", "1245", "0")
     print(f"Manager bonus: {manager2.get_bonus()}")
 
-    # client = Client("Maria", "999.999.999-45", "548")
-
-
-
-
     control = Bonus_Control()
     control.register(employee)
     control.register(manager1)
     control.register(manager2)
-    # control.register(client)
 
     print(f"Total: {control.bonus_total}")
